ea9b2e007505854bef2a4bb819615764790073c1
PedroBlue13 <pedroblue@outlook.com.br>
2024-11-25 21:10:25 -0300

    temperatura

---
+++ intro_python/temperatura.py
@@ -0,0 +1,8 @@
+#Receba uma tempetarua em garenheit e exiba em graus celsius
+# c = 5 * f - 32/9
+
+
+fahrenheit = float(input("Digite uma temperatura em fahrenheit:"))
+
+celsius = 5 * ((fahrenheit -32)/9)
+print(f'A temperatura em celsius é de: {celsius}')

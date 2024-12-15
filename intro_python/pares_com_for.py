0924404fe75b97b547cf437d11b83e0b1bf08b97
PedroBlue13 <pedroblue@outlook.com.br>
2024-12-15 19:42:08 -0300

    nº par com for

---
+++ intro_python/pares_com_for.py
@@ -0,0 +1,12 @@
+#Exiba todos os números pares entre 1 e 100 usando for
+
+
+
+for i in range (0, 1002, 2):
+    print(i)
+    
+    
+    
+for i in range (1, 1000):
+    if i %2 == 0:
+        print(i)

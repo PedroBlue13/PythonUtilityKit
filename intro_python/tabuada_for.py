f44be0fca994afd50f1ba350a0a8548ede99a4a4
PedroBlue13 <pedroblue@outlook.com.br>
2024-12-15 19:36:04 -0300

    tabuada com for

---
+++ intro_python/tabuada_for.py
@@ -0,0 +1,5 @@
+#Rebeba um número e mostre a tabuada dele usando o laço for
+
+n1 = int(input("Digite um número:\n"))
+for i in range (11):
+      print  (f'A tabuada do número {n1} até o 10 é \n  {n1} x {i} = {n1*i}  ')

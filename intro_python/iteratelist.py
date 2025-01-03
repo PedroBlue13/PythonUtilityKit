5643a042a6ed935a9f8ea7b393f595cfd3e882e2
PedroBlue13 <pedroblue@outlook.com.br>
2025-01-03 17:52:17 -0300

    add

--- intro_python/iteratelist.py
+++ intro_python/iteratelist.py
@@ -5,3 +5,12 @@ dias = [1,3,2,5,7,6]
 for i in range(0, len(dias)):
     print(dias[i], i)
     print(f'Dia: {dias[i]} Posição: {i}')
+    
+    
+#forma pythonica
+for i in dias:
+    print(i)
+    
+#mais pythonica ainda
+x = list(enumerate(dias)) 
+print(f'valor de x{x}')   
